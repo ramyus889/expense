90b0f747c404508a2618a929df3046df5c58a0cb
ramyus889 <ramyus889.com>
2024-01-31 17:53:00 +0300

    Добавлен

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  baseUrl: "/expense/",
+  base: "/expense/",
   publicPath: "/expense/",
   plugins: [vue()],
   resolve: {
